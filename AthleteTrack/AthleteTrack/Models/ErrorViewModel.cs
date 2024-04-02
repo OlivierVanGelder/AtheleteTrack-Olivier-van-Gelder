58e8763f3f51f79b9734c0654b942c2f69d980e6
OlivierVanGelder <150012161+OlivierVanGelder@users.noreply.github.com>
2024-04-02 08:44:50 +0200

    Nieuw project aangemaakt

---
+++ AthleteTrack/AthleteTrack/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AthleteTrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
